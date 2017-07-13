ab3d4286694e1972bf62c708d6a86414a95a75a5
Sahl Rowther <sahl_rs@yahoo.com>
2017-07-13 20:34:29 +0100

    Improved Efficiency

--- 8ConnectivityTest.py
+++ 8ConnectivityTest.py
@@ -11,154 +11,349 @@ import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 import copy
+import os
 from functools import reduce
-imgs = glob.glob('*.fits')
+import time
+imgs = glob.glob('5*.fits')
 
-pic = np.array([[1,1,0,1,1,1,0,1],
-               [1,1,0,1,0,1,0,1],
-               [1,1,1,1,0,0,0,1],
-               [0,0,0,0,0,0,0,1],
-               [1,1,1,1,0,1,0,1],
-               [0,0,0,1,0,1,0,1],
-               [1,1,1,1,0,0,0,1],
-               [1,1,1,1,0,1,1,1]])
-# plt.imshow(pic, cmap='hot')
 
-pic = Data_analysis.smooth_image(imgs[0])
-# pic = fits.open(imgs[0])[0].data
+def isolateGalaxy(image):
 
-labels = np.zeros_like(pic)
-label_count = 0
+    pic = np.array([[1,1,0,1,1,1,0,1],
+                   [1,1,0,1,0,1,0,1],
+                   [1,1,1,1,0,0,0,1],
+                   [0,0,0,0,0,0,0,1],
+                   [1,1,1,1,0,1,0,1],
+                   [0,0,0,1,0,1,0,1],
+                   [1,1,1,1,0,0,0,1],
+                   [1,1,1,1,0,1,1,1]])
+    # plt.imshow(pic, cmap='hot')
 
-# print pic.shape
+    pic = Data_analysis.smooth_image(image)
+    # pic = fits.open(imgs[0])[0].data
 
-# for i in range(-1,2,1):
-#     print 3+i
+    labels = np.zeros_like(pic)
+    label_count = 0
 
-for i in range(pic.shape[1]):
-    for j in range(pic.shape[0]):
-        if pic[i,j]<=30:
-            pass
-        else:
-            if label_count==0:
-                labels[i,j]=1
-                label_count+=1
+    # print pic.shape
+
+    # for i in range(-1,2,1):
+    #     print 3+i
+
+    for i in range(pic.shape[1]):
+        for j in range(pic.shape[0]):
+            if pic[i,j]<=35:
+                pass
             else:
-                if i==0:
-                    if labels[i,j-1]!=0:
-                        labels[i,j]=labels[i,j-1]
-                    else:
-                        label_count+=1
-                        labels[i,j]=label_count
+                if label_count==0:
+                    labels[i,j]=1
+                    label_count+=1
                 else:
-                    nearest_neighbours = []
-                    for k in range(-1,2,1):
-                        if j+k<0 or j+k>7:
+                    if i==0:
+                        if labels[i,j-1]!=0:
+                            labels[i,j]=labels[i,j-1]
+                        else:
+                            label_count+=1
+                            labels[i,j]=label_count
+                    else:
+                        nearest_neighbours = []
+                        for k in range(-1,2,1):
+                            if j+k<0 or j+k>7:
+                                pass
+                            else:
+                                nearest_neighbours.append(labels[i-1, j+k])
+                        if j-1>=0:
+                            nearest_neighbours.append(labels[i,j-1])
+                        nearest_neighbours = np.array(nearest_neighbours)
+                        nearest_neighbours = ma.masked_array(nearest_neighbours, nearest_neighbours==0, fill_value=99)
+
+                        if nearest_neighbours.all() is np.ma.masked:
+                            label_count+=1
+                            labels[i,j] = label_count
+                        else:
+                            labels[i,j] = np.min(nearest_neighbours)
+    #                     print nearest_neighbours, labels[i,j], (i,j)
+
+    size = labels.shape[0]
+
+    labels_copy = copy.deepcopy(labels)
+    # x=6
+
+    for x in range(2,label_count+1):
+
+        masks = [labels==0, labels>x]
+        total_mask = reduce(np.logical_or, masks)
+        labels_copy = ma.masked_array(labels, mask=total_mask)
+
+        # print np.min(labels_copy)
+
+        current_label = x
+        min_label_connected = current_label
+        mins = []
+
+        # print labels
+
+        for i in range(labels.shape[1]):
+            for j in range(labels.shape[0]):
+                if labels[i,j]!=0:
+                    nn = []
+
+                    # current pixel
+            #             print(i,j)
+
+                    # vertical and horizontal neighbours
+                    if i!=size-1:
+            #             print i+1,j
+                        nn.append(labels_copy[i+1,j])
+                    if i>0:
+            #             print i-1,j
+                        nn.append(labels_copy[i-1,j])
+                    if j!=size-1:
+            #             print i,j+1
+                        nn.append(labels_copy[i,j+1])
+                    if j>0:
+            #             print i,j-1
+                        nn.append(labels_copy[i,j-1])
+                    # diagonal neighbours
+                    if i!=size-1:
+                        if j!=size-1:
+            #                 print i+1,j+1
+                            nn.append(labels_copy[i+1,j+1])
+                        if j>0:
+            #                 print i+1,j-1
+                            nn.append(labels_copy[i+1,j-1])
+                    if j!=size-1:
+                        if i>0:
+            #                 print i-1,j+1
+                            nn.append(labels_copy[i-1,j+1])
+                    if i>0 and j>0:
+            #             print i-1,j-1
+                        nn.append(labels_copy[i-1,j-1])
+
+            #             print 'labels of nearest neighbours: ', nn
+                    nn = np.array(nn)
+                    nn = ma.masked_invalid(nn)
+            #             print nn.all is np.ma.masked
+                    if current_label in nn:
+                        if nn.all() is np.ma.masked:
                             pass
                         else:
-                            nearest_neighbours.append(labels[i-1, j+k])
-                    if j-1>=0:
-                        nearest_neighbours.append(labels[i,j-1])
-                    nearest_neighbours = np.array(nearest_neighbours)
-                    nearest_neighbours = ma.masked_array(nearest_neighbours, nearest_neighbours==0, fill_value=99)
+                            mins.append(np.min(nn))
 
-                    if nearest_neighbours.all() is np.ma.masked:
-                        label_count+=1
-                        labels[i,j] = label_count
+        mins = np.array(mins)
+#         print(mins)
+        if len(mins)==0:
+            pass
+    #         print current_label
+        else:
+    #         print np.min(np.array(mins))
+            min_label_connected = np.min(np.array(mins))
+
+        for i in range(labels.shape[1]):
+            for j in range(labels.shape[0]):
+                if labels[i,j]==current_label:
+                    labels[i,j] = min_label_connected
+
+
+
+#     print labels[128,128]
+
+    mask = [labels!=labels[128,128]]
+    labels = ma.masked_array(labels, mask=mask)
+
+#     Data_analysis.plot_image(labels)
+
+    pic = ma.masked_array(pic, mask=mask)
+    pic = ma.filled(pic, 0)
+    Data_analysis.plot_image(pic)
+
+#     plt.show()
+
+#     print 'Plots'
+
+    hdu = fits.PrimaryHDU(pic)
+    hdulist = fits.HDUList([hdu])
+    output_name = 'Isolated_'+image
+    if len(glob.glob(output_name)) != 0:
+        os.remove(output_name)
+    hdulist.writeto('Isolated_'+image)
+
+#     print 'Saved'
+
+#     Data_analysis.plot_image(pic)
+#     plt.figure()
+#     plt.imshow(pic, cmap='hot')
+
+
+def GalaxyIsolation(image):
+    """
+    For the given image, gives each pixel a label by starting with the top left pixel.
+    Then goes to the nearest pixel in the next column and labels (if it is above a
+    threshold) it by comparing it with it nearest labelled neighbours. Then repeats for
+    each row.
+
+    Once everything is labeled, it determines which labels are connected to each other.
+    Starts at the 1st label (1) [which is a parent label]
+    is and finds all the nearest neighbours of every pixel with the label (1). Any pixels
+    with a label other than (1) is clearly connected to (1) [which are child labels].
+    Then the child labels are all re-labeled (1). Then it moves onto the parent label.
+    And so on until only parent labels remain.
+    """
+    pic = np.array([[1,1,0,1,1,1,0,1],
+                   [1,1,0,1,0,1,0,1],
+                   [1,1,1,1,0,0,0,1],
+                   [0,0,0,0,0,0,0,1],
+                   [1,1,1,1,0,1,0,1],
+                   [0,0,0,1,0,1,0,1],
+                   [1,1,1,1,0,0,0,1],
+                   [1,1,1,1,0,1,1,1]])
+    # plt.imshow(pic, cmap='hot')
+
+    pic = Data_analysis.smooth_image(image)
+
+    labels = np.zeros_like(pic)
+    label_count = 0
+
+    for i in range(pic.shape[1]):
+        for j in range(pic.shape[0]):
+            if pic[i,j]<=37:
+                pass
+            else:
+                if label_count==0:
+                    labels[i,j]=1
+                    label_count+=1
+                else:
+                    if i==0:
+                        if labels[i,j-1]!=0:
+                            labels[i,j]=labels[i,j-1]
+                        else:
+                            label_count+=1
+                            labels[i,j]=label_count
                     else:
-                        labels[i,j] = np.min(nearest_neighbours)
-#                     print nearest_neighbours, labels[i,j], (i,j)
+                        nearest_neighbours = []
+                        for k in range(-1,2,1):
+                            if j+k<0 or j+k>7:
+                                pass
+                            else:
+                                nearest_neighbours.append(labels[i-1, j+k])
+                        if j-1>=0:
+                            nearest_neighbours.append(labels[i,j-1])
+                        nearest_neighbours = np.array(nearest_neighbours)
+                        nearest_neighbours = ma.masked_array(nearest_neighbours, nearest_neighbours==0, fill_value=99)
 
-size = labels.shape[0]
+                        if nearest_neighbours.all() is np.ma.masked:
+                            label_count+=1
+                            labels[i,j] = label_count
+                        else:
+                            labels[i,j] = np.min(nearest_neighbours)
+    #                     print nearest_neighbours, labels[i,j], (i,j)
 
-labels_copy = copy.deepcopy(labels)
-# x=6
+#     Data_analysis.plot_image(labels, cmin=0, cmax=np.max(labels))
 
-for x in range(2,label_count+1):
+    size = labels.shape[0]
 
-    masks = [labels==0, labels>x]
-    total_mask = reduce(np.logical_or, masks)
-    labels_copy = ma.masked_array(labels, mask=total_mask)
+    skip = []
 
-    # print np.min(labels_copy)
+    labels_copy = copy.deepcopy(labels)
+    for l in range(1,size):
+        if l not in skip:
+            masks = [labels==0, labels!=l]
+            total_mask = reduce(np.logical_or, masks)
+            label_mask = ma.masked_array(labels, mask=total_mask)
+            label_mask = ma.filled(label_mask, 0)
 
-    current_label = x
-    min_label_connected = current_label
-    mins = []
-
-    # print labels
-
-    for i in range(labels.shape[1]):
-        for j in range(labels.shape[0]):
-            if labels[i,j]!=0:
-                nn = []
-
-                # current pixel
-        #             print(i,j)
-                # vertical and horizontal neighbours
+            nonzero_locations = np.transpose(np.nonzero(label_mask))
+            nn = []
+            for coord in nonzero_locations:
+                i = coord[0]
+                j = coord[1]
+#                 print(labels_copy[i,j])
+#                 vertical and horizontal neighbours
                 if i!=size-1:
-        #             print i+1,j
                     nn.append(labels_copy[i+1,j])
+#                     print(labels_copy[i+1,j])
                 if i>0:
-        #             print i-1,j
                     nn.append(labels_copy[i-1,j])
+#                     print(labels_copy[i-1,j])
                 if j!=size-1:
-        #             print i,j+1
                     nn.append(labels_copy[i,j+1])
+#                     print(labels_copy[i,j+1])
                 if j>0:
-        #             print i,j-1
                     nn.append(labels_copy[i,j-1])
+#                     print(labels_copy[i,j-1])
                 # diagonal neighbours
                 if i!=size-1:
                     if j!=size-1:
-        #                 print i+1,j+1
                         nn.append(labels_copy[i+1,j+1])
+#                         print(labels_copy[i+1,j+1])
                     if j>0:
-        #                 print i+1,j-1
                         nn.append(labels_copy[i+1,j-1])
+#                         print(labels_copy[i+1,j-1])
                 if j!=size-1:
                     if i>0:
-        #                 print i-1,j+1
                         nn.append(labels_copy[i-1,j+1])
+#                         print(labels_copy[i-1,j+1])
                 if i>0 and j>0:
-        #             print i-1,j-1
                     nn.append(labels_copy[i-1,j-1])
+#                     print(labels_copy[i-1,j-1])
 
-        #             print 'labels of nearest neighbours: ', nn
-                nn = np.array(nn)
-                nn = ma.masked_invalid(nn)
-        #             print nn.all is np.ma.masked
-                if current_label in nn:
-                    if nn.all() is np.ma.masked:
-                        pass
-                    else:
-                        mins.append(np.min(nn))
+#                     print 'labels of nearest neighbours: ', nn
+            nn = np.array(nn)
+#             print('Here')
+#             print(np.unique(nn))
+            connected_labels = np.unique(nn)
 
-    mins = np.array(mins)
-    print(mins)
-    if len(mins)==0:
-        pass
-#         print(current_label)
-    else:
-#         print(np.min(np.array(mins)))
-        min_label_connected = np.min(np.array(mins))
+            connected_labels = connected_labels[connected_labels!=0]
+#             print(connected_labels)
+            for c in connected_labels:
+                if c not in skip:
+                    skip.append(c)
+                labels_copy[labels_copy == c] = l
+#             print(labels_copy)
+#     print(skip)
+#     Data_analysis.plot_image(labels_copy, cmin=0, cmax=np.max(labels_copy))
 
-    for i in range(labels.shape[1]):
-        for j in range(labels.shape[0]):
-            if labels[i,j]==current_label:
-                labels[i,j] = min_label_connected
+    labels = labels_copy
+
+    mask = [labels!=labels[128,128]]
+    labels = ma.masked_array(labels, mask=mask)
+
+    pic = ma.masked_array(pic, mask=mask)
+    pic = ma.filled(pic, 0)
+    Data_analysis.plot_image(pic)
+
+    hdu = fits.PrimaryHDU(pic)
+    hdulist = fits.HDUList([hdu])
+    output_name = 'Isolated_'+image
+    if len(glob.glob(output_name)) != 0:
+        os.remove(output_name)
+    hdulist.writeto('Isolated_'+image)
+
+#     plt.show()
 
 
+# GalaxyIsolation(imgs[5])
 
-print("")
-print(labels[128,128])
+t1 = time.clock()
 
-mask = [labels!=labels[128,128]]
-labels = ma.masked_array(labels, mask=mask)
+for i,img in enumerate(imgs):
+    GalaxyIsolation(img)
+    print('Image '+str(i+1)+' processed.')
 
-# print labels
-plt.figure()
-plt.imshow(labels, cmap='hot')
-plt.figure()
-plt.imshow(pic, cmap='hot')
+t2 = time.clock()
+print(t2-t1)
+
+# t1 = time.clock()
+#
+# for i,img in enumerate(imgs):
+#     isolateGalaxy(img)
+#     print('Image '+str(i+1)+' processed.')
+#
+# t2 = time.clock()
+# print(t2-t1)
+
 plt.show()
+# isolateGalaxy(imgs[4])
+
+
+# plt.show()
--- Data_analysis.py
+++ Data_analysis.py
@@ -9,13 +9,22 @@ import matplotlib.pyplot as plt
 import numpy.ma as ma
 import numpy as np
 import glob
-#import skimage
-from numpy.core import multiarray
-#import cv2
+import cv2
+import scipy.misc# import skimage
+# import cv2
 
-def plot_image(image_data, cmin=0, cmax=100, cmap='hot'):
+def rotateImage(image, angle):
+    row,col = image.shape
+    center=tuple(np.array([128,128]))
+    rot_mat = cv2.getRotationMatrix2D(center,angle,1.0)
+    new_image = cv2.warpAffine(image, rot_mat, image.shape[0:2],flags=cv2.INTER_LINEAR)
+    return new_image
+
+def plot_image(image_data, cmin=0, cmax=100, cmap='hot',axis=None):
     plt.figure()
     plt.imshow(image_data, clim=[cmin,cmax], cmap=cmap)
+    if axis is not None:
+        plt.axis(axis)
     plt.xlabel('x pixel')
     plt.ylabel('y pixel')
     plt.tight_layout()
@@ -38,8 +47,8 @@ def smooth_image(image):
                 moving_avg_img[x,y] = moving_avg_img[x,y]/count
 
 
-    print(np.mean(image_data))
-    print(np.median(image_data))
+#     print(np.mean(image_data))
+#     print(np.median(image_data))
 
 
     difference = image_data-moving_avg_img
@@ -47,31 +56,61 @@ def smooth_image(image):
 
 #     plot_image(input_plus_difference)
 #     plot_image(image_data)
-    plot_image(moving_avg_img)
+#     plot_image(moving_avg_img)
 
     return moving_avg_img
 #     plt.show()
 
-imgs = glob.glob('*.fits')
+imgs = glob.glob('5*.fits')
+print(imgs)
+isolated_galaxies = glob.glob('Isolated_*.fits')
+print(isolated_galaxies)
 
-def light_profile(image):
+# def light_profile(image):
+#
+#     image_data = smooth_image(image)
+#     x_data, y_data = np.arange(0, image_data.shape[1]), np.arange(0, image_data.shape[0])
+#
+#     x_cent, y_cent = 128, 128
+#     for r in range(10,150,5):
+#         print(r)
+#
+#         data_x, data_y = np.meshgrid(x_data, y_data)
+#         mask = [(np.sqrt((data_x-x_cent)**2 + (data_y-y_cent)**2)>r)]
+#
+#         plot_image(ma.MaskedArray(image_data, mask=mask))
+#         plt.show()
 
-    image_data = smooth_image(image)
-    x_data, y_data = np.arange(0, image_data.shape[1]), np.arange(0, image_data.shape[0])
-
-    x_cent, y_cent = 128, 128
-    for r in range(10,150,5):
-        print(r)
-
-        data_x, data_y = np.meshgrid(x_data, y_data)
-        mask = [(np.sqrt((data_x-x_cent)**2 + (data_y-y_cent)**2)>r)]
-
-        plot_image(ma.MaskedArray(image_data, mask=mask))
-        plt.show()
+def find_radius(image_data):
+    ymin = np.min(np.nonzero(image_data)[0][:])
+    ymax = np.max(np.nonzero(image_data)[0][:])
+    xmin = np.min(np.nonzero(image_data)[:][1])
+    xmax = np.max(np.nonzero(image_data)[:][1])
+    return np.max(np.abs(np.array([ymin, ymax, xmin, xmax])-128))
+#     return np.max(np.abs(np.array([xmin,xmax])-128)), np.max(np.abs(np.array([ymin,ymax])-128))
 
 
-# edges = cv2.Canny(img,100,200)
+# find_radius(isolated_galaxies[3])
 
-# light_profile(imgs[2])
+def flip_image(image):
+    image_data = fits.open(image)[0].data
+    flipped_data = image_data[::-1,::-1]
+    flipped_scipy = scipy.misc.imrotate(image_data, 180, interp='nearest')
+#     flipped_numpy = np.fliplr(image_data)
+    f = rotateImage(image_data, 180)
+    plot_image(image_data=image_data)
+#     plot_image(f,cmin=None, cmax=None)
+    plot_image(flipped_data)
+#     plot_image(flipped_scipy)
+#     plot_image(flipped_numpy)
+    diff = np.abs(image_data-flipped_data)
+    rad = find_radius(diff)+5
+#     print(rad)
+    mask = diff==0
+    plot_image(ma.masked_array(diff, mask=mask),cmax=np.max(diff), cmin=0, cmap='Greys', axis=(128-rad,128+rad,128-rad,128+rad))
 
-# smooth_image(imgs[2])
+
+# plot_image(smooth_image(imgs[5]))
+# flip_image(isolated_galaxies[5])
+# plt.show()
+
