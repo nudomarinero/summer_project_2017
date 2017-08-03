f8bd415237d1893746651a748f6ef9adaa993160
Sahl Rowther <sahl_rs@yahoo.com>
2017-08-03 14:08:11 +0100

    Docstrings explaining images

--- test/test_star_detection.py
+++ test/test_star_detection.py
@@ -23,80 +23,248 @@ from astropy.io import fits
 
 sys.path.append('/Users/Sahl/Desktop/University/Year_Summer_4/Summer_code/summer_project_2017/')
 # print(sys.path)
-from Image_Analysis import image_analysis
+from Image_Analysis import image_analysis, detect_star
 
 file_dir = '/Users/Sahl/Desktop/University/Year_Summer_4/Summer_Project/Data/'
 
 class MyTest(unittest.TestCase):
-    def test_588013382727958540(self):
-        # Test image: 588013382727958540.fits
+    def test_is_star_in_588013382727958540(self):
+        """
+        Checks if there is a star in image 588013382727958540.fits. This image
+        has 2 objects that are clearly galaxies, and 1 small and very circular
+        object with no clear diffraction spikes. Thus even if the small object
+        is a star, it is not relevant.
+
+        This test will fail if a star is detected.
+        """
         output = image_analysis(file_dir+'588013382727958540.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587739609175031857(self):
-        # Test image: 587739609175031857.fits
+    def test_is_star_in_587739609175031857(self):
+        """
+        Checks if there is a star in image 587739609175031857.fits. This image
+        has 2 objects that are clearly galaxies; where 1 galaxy is small while
+        the other is large. This image has no stars.
+
+        This test will fail if a star is detected.
+        """
         output = image_analysis(file_dir+'587739609175031857.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)    
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587727213348520295(self):
-        # Test image: 587727213348520295.fits.fits
+    def test_is_star_in_587727213348520295(self):
+        """
+        Checks if there is a star in image 587727213348520295.fits. This image
+        has a large spiral galaxy with a bright center and very small local
+        maxima. This image has no stars.
+
+        This test will fail if a star is detected.
+        """       
         output = image_analysis(file_dir+'587727213348520295.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587742061616758804(self):
-        # Test image: 587742061616758804.fits.fits
+    def test_is_star_in_587742061616758804(self):
+        """
+        Checks if there is a star in image 587742061616758804.fits. This image
+        has a large spiral galaxy with a faint center next to a very bright
+        star with diffraction spikes.
+
+        This test will fail if a star is not detected.
+        """
         output = image_analysis(file_dir+'587742061616758804.fits')
-        self.assertEqual(output[-1], True)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, True, msg='Star not detected when it should have been.')
 
-    def test_587736619321655535(self):
-        # Test image: 587736619321655535.fits.fits
+    def test_is_star_in_587736619321655535(self):
+        """
+        Checks if there is a star in image 587736619321655535.fits. This image
+        has multiple large objects with small bright centers and large halos.
+        This image has no stars.
+
+        This test will fail if a star is detected.
+        """
         output = image_analysis(file_dir+'587736619321655535.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587739167310807244(self):
-        # Test image: 587742061616758804.fits.fits
+    def test_is_star_in_587739167310807244(self):
+        """
+        Checks if there is a star in image 587739167310807244.fits. This image
+        has 2 large objects that are clearly galaxies. This image has no stars.
+
+        This test will fail if a star is detected.
+        """
         output = image_analysis(file_dir+'587739167310807244.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587733603734388952(self):
-        # Test image:587733603734388952.fits.fits
+    def test_is_star_in_587733603734388952(self):
+        """
+        Checks if there is a star in image 587733603734388952.fits. This image
+        has a large object with multiple nuclei at the center. There is also a 
+        large bright star with clear diffraction spikes next to it.
+
+        This test will fail if a star is not detected.
+        """
         output = image_analysis(file_dir+'587733603734388952.fits')
-        self.assertEqual(output[-1], True)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, True, msg='Star not detected when it should have been.')
 
-    def test_587739406805762069(self):
-        # Test image: 587739406805762069.fits
+    def test_is_star_in_587739406805762069(self):
+        """
+        Checks if there is a star in image 587739406805762069.fits. This image
+        has 2 large objects that are clearly galaxies. This image has no stars.
+
+        This test will fail if a star is detected.
+        """
         output = image_analysis(file_dir+'587739406805762069.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587734621629513866(self):
-        # Test image: 587734621629513866.fits. Test case with only 1 maxima
+    def test_is_star_in_587734621629513866(self):
+        """
+        Checks if there is a star in image 587734621629513866.fits. This image
+        has only a galaxy with a single maxima. This image has no stars.
+
+        This test will fail if a star is detected.
+        """
         output = image_analysis(file_dir+'587734621629513866.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587744728761761895(self):
-        # Test image: 587744728761761895.fits
+    def test_is_star_in_587744728761761895(self):
+        """
+        Checks if there is a star in image 587744728761761895.fits. This image
+        has a large irregular object at the center. This image has no stars.
+
+        This test will fail if a star is detected.
+        """    
         output = image_analysis(file_dir+'587744728761761895.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587742566784106610(self):
-        # Test image: 587742566784106610.fits
+    def test_is_star_in_587742566784106610(self):
+        """
+        Checks if there is a star in image 587742566784106610.fits. This image
+        has 2 small objects that are galaxies. This image has no stars.
+
+        This test will fail if a star is detected.
+        """
         output = image_analysis(file_dir+'587742566784106610.fits')
-        self.assertEqual(output[-1], False)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, False, msg='Star detected when it should not have been.')
 
-    def test_587730847428968484(self):
-        # Test image: 587730847428968484.fits . Previously a false negativs
+    def test_is_star_in_587730847428968484(self):
+        """
+        Checks if there is a star in image 587730847428968484.fits. This image
+        has a large elliptical galaxy with a faint center next to a very bright
+        star with diffraction spikes.
+
+        This test will fail if a star is not detected.
+        """
         output = image_analysis(file_dir+'587730847428968484.fits')
-        self.assertEqual(output[-1], True)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, True, msg='Star not detected when it should have been.')
 
-    def test_587742572149080091(self):
-        # Test image: 587730847428968484.fits . Previously a false negativs
-        output = image_analysis(file_dir+'587730847428968484.fits')
-        self.assertEqual(output[-1], True)
+    def test_is_star_in_587742572149080091(self):
+        """
+        Checks if there is a star in image 587742572149080091.fits. This image
+        has 3 small circular objects. One of them is a bright star with clear
+        diffrection spikes. One is a very small circular object. And at the
+        center is a slightly faint galaxy the same size as the star.
 
-    def test_588017111293296655(self):
-        # Test image: 588017111293296655.fits . 
+        This test will fail if a star is not detected.
+        """
+        output = image_analysis(file_dir+'587742572149080091.fits')
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, True, msg='Star not detected when it should have been.')
+
+    def test_is_star_in_588017111293296655(self):
+        """
+        Checks if there is a star in image 588017111293296655.fits. This image
+        has a small circular galaxy at the center. It also has a very large star
+        with large diffraction spikes.
+
+        This test will fail if a star is not detected.
+        """
         output = image_analysis(file_dir+'588017111293296655.fits')
-        self.assertEqual(output[-1], True)
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, True, msg='Star not detected when it should have been.')
+
+    def test_is_star_in_588007006334943294(self):
+        """
+        Checks if there is a star in image 588007006334943294.fits. This image
+        has a galaxy with a small bright center, but a huge halo. This image
+        also has a small bright star with diffraction spikes within tht edges of
+        the galaxy halo.
+
+        This test will fail if a star is not detected.
+        """
+        output = image_analysis(file_dir+'588007006334943294.fits')
+        min_asmmetry_flux, maxima, galaxy_name, galaxy = output[5], output[1], output[0], output[-1]
+        if min_asmmetry_flux < 0.25 or len(maxima) == 1:
+            detect_status = False
+        else:
+            detect_status = detect_star(galaxy)  
+        self.assertEqual(detect_status, True, msg='Star not detected when it should have been.')
 
 if __name__ == '__main__':
-   unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
